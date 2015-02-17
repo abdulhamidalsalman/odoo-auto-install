129c528ae65eb6e6bf6e600bc235961cb0b3da65
Andreas Santoro <Andreas.Santoro@pentlandfirth.com>
2015-02-17 21:41:59 +0100

    cleanup

--- install_odoo.sh
+++ install_odoo.sh
@@ -3,41 +3,37 @@
 # Script for Installation: ODOO Saas4/Trunk server on Ubuntu 14.04 LTS
 # Author: Andras Santoro
 #-------------------------------------------------------------------------------
-#  
-# This script will install ODOO Server on
-# clean Ubuntu 14.04 Server
+# This script will install ODOO Server on clean Ubuntu 14.04 Server
 # - added Nginx Proxy Server
 #-------------------------------------------------------------------------------
-# USAGE:
-#
-# odoo-install
-#
 # EXAMPLE:
-# ./odoo-install 
-#
+# sudo ./install_odoo.sh
 ################################################################################
- 
-##fixed parameters
-#openerp
 
-# Run apt-get update/upgrade
-OE_UPGRADESERVER="false"
+# Update and Upgrade the Ubuntu Server
+ODOO_SERVER_UPGRADESERVER=			"false"
 
-OE_USER="odoo"
-OE_HOME="/opt/$OE_USER"
-OE_HOME_EXT="/opt/$OE_USER/$OE_USER-server"
+#--ODOO 
+ODOO_GIT_VERSION=					"8.0"									#The version from GITHUB to install "8.0" / "7.0" / "master" / "saas-4"...
+ODOO_USER=							"odoo"									#This is the user odoo is running under
+ODOO_INSTALL_DIRECTORY=				"/opt/odoo"								#Install directory
+ODOO_INSTALL_DIRECTORY_EXT=			"/opt/odoo/odoo-server"					#Install directory odoo server
+ODOO_SERVER_ADMIN_PASSWORD=			"!superadmin123"						#The admin password for odoo after installation
+ODOO_CONFIGFILE_NAME=				"odoo-server"							#The config file name "odoo-server.conf"
 
-#Enter version for checkout "7.0" for version 7.0, "saas-4, saas-5 (opendays version) and "master" for trunk
-OE_VERSION="8.0"
+#--PostgresSQL
+ODOO_SERVER_POSTGRES_INSTALL=		"true"									#Install Postgress database server
+ODOO_SERVER_POSTGRES_PASSWORD=		"$ODOO_SERVER_ADMIN_PASSWORD"			#Database server password
 
-#set the superadmin password
-OE_SUPERADMIN="!postPLFAdmin2014odoo"
-OE_CONFIG="$OE_USER-server"
+#--NGINX
+ODOO_SERVER_NGINX_INSTALL=			"true"									#Install 
+ODOO_SERVER_NGINX_URL=				"https://odoo.mycompany.com"
+ODOO_SERVER_NGINX_PORT=				"443"
 
 #--------------------------------------------------
 # Update Server
 #--------------------------------------------------
-if [ $OE_UPGRADESERVER == "true" ]
+if [ $ODOO_SERVER_UPGRADESERVER == "true" ]
 then
 	echo -e "\n---- Update Server ----"
 	sudo apt-get update
@@ -49,15 +45,19 @@ fi
 #--------------------------------------------------
 # Install PostgreSQL Server
 #--------------------------------------------------
-echo -e "\n---- Install PostgreSQL Server ----"
-sudo apt-get install postgresql -y
-	
-echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
-sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postgresql/9.3/main/postgresql.conf
+if [ $ODOO_SERVER_UPGRADESERVER == "true" ]
+then
+	echo -e "\n---- Install PostgreSQL server ----"
+	sudo apt-get install postgresql -y
+		
+	echo -e "\n---- PostgreSQL $PG_VERSION Settings  ----"
+	sudo sed -i s/"#listen_addresses = 'localhost'"/"listen_addresses = '*'"/g /etc/postgresql/9.3/main/postgresql.conf
 
-echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
-sudo su - postgres -c "createuser -s $OE_USER" 2> /dev/null || true
-
+	echo -e "\n---- Creating the ODOO PostgreSQL User  ----"
+	sudo su - postgres -c "createuser -s $ODOO_USER" 2> /dev/null || true
+else
+	echo -e "\n---- PostgreSQL skipped ----"
+fi
 #--------------------------------------------------
 # Install Dependencies
 #--------------------------------------------------
@@ -72,11 +72,11 @@ echo -e "\n---- Install python libraries ----"
 sudo pip install gdata
 	
 echo -e "\n---- Create ODOO system user ----"
-sudo adduser --system --quiet --shell=/bin/bash --home=$OE_HOME --gecos 'ODOO' --group $OE_USER
+sudo adduser --system --quiet --shell=/bin/bash --home=$ODOO_INSTALL_DIRECTORY --gecos 'ODOO' --group $ODOO_USER
 
 echo -e "\n---- Create Log directory ----"
-sudo mkdir /var/log/$OE_USER
-sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
+sudo mkdir /var/log/$ODOO_USER
+sudo chown $ODOO_USER:$ODOO_USER /var/log/$ODOO_USER
 
 
 #--------------------------------------------------
@@ -84,122 +84,121 @@ sudo chown $OE_USER:$OE_USER /var/log/$OE_USER
 #--------------------------------------------------
 sudo wget http://downloads.sourceforge.net/project/wkhtmltopdf/0.12.1/wkhtmltox-0.12.1_linux-trusty-amd64.deb
 sudo dpkg -i wkhtmltox-0.12.1_linux-trusty-amd64.deb
-# Todo copy files to /usr/local/bin
+# Todo: copy files to /usr/local/bin
 
 
 #--------------------------------------------------
 # Install ODOO
 #--------------------------------------------------
 echo -e "\n==== Installing ODOO Server ===="
-sudo git clone --branch $OE_VERSION https://www.github.com/odoo/odoo $OE_HOME_EXT/
+sudo git clone --branch $ODOO_GIT_VERSION https://www.github.com/odoo/odoo $ODOO_INSTALL_DIRECTORY_EXT/
 
 echo -e "\n---- Create custom module directory ----"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom"
-sudo su $OE_USER -c "mkdir $OE_HOME/custom/addons"
+sudo su $ODOO_USER -c "mkdir $ODOO_INSTALL_DIRECTORY/custom"
+sudo su $ODOO_USER -c "mkdir $ODOO_INSTALL_DIRECTORY/custom/addons"
 
 echo -e "\n---- Setting permissions on home folder ----"
-sudo chown -R $OE_USER:$OE_USER $OE_HOME/*
+sudo chown -R $ODOO_USER:$ODOO_USER $ODOO_INSTALL_DIRECTORY/*
 
-echo -e "* Create server config file"
-sudo cp $OE_HOME_EXT/debian/openerp-server.conf /etc/$OE_CONFIG.conf
-sudo chown $OE_USER:$OE_USER /etc/$OE_CONFIG.conf
-sudo chmod 640 /etc/$OE_CONFIG.conf
+echo -e "\n---- Create server config file"
+sudo cp $ODOO_INSTALL_DIRECTORY_EXT/debian/openerp-server.conf /etc/$ODOO_CONFIGFILE_NAME.conf
+sudo chown $ODOO_USER:$ODOO_USER /etc/$ODOO_CONFIGFILE_NAME.conf
+sudo chmod 640 /etc/$ODOO_CONFIGFILE_NAME.conf
 
-echo -e "* Change server config file"
-sudo sed -i s/"db_user = .*"/"db_user = $OE_USER"/g /etc/$OE_CONFIG.conf
-sudo sed -i s/"; admin_passwd.*"/"admin_passwd = $OE_SUPERADMIN"/g /etc/$OE_CONFIG.conf
-sudo su root -c "echo 'logfile = /var/log/$OE_USER/$OE_CONFIG$1.log' >> /etc/$OE_CONFIG.conf"
-sudo su root -c "echo 'addons_path=$OE_HOME_EXT/addons,$OE_HOME/custom/addons' >> /etc/$OE_CONFIG.conf"
+echo -e "\n---- Change server config file"
+sudo sed -i s/"db_user = .*"/"db_user = $ODOO_USER"/g /etc/$ODOO_CONFIGFILE_NAME.conf
+sudo sed -i s/"; admin_passwd.*"/"admin_passwd = $ODOO_SERVER_ADMIN_PASSWORD"/g /etc/$ODOO_CONFIGFILE_NAME.conf
+sudo su root -c "echo 'logfile = /var/log/$ODOO_USER/$ODOO_CONFIGFILE_NAME$1.log' >> /etc/$ODOO_CONFIGFILE_NAME.conf"
+sudo su root -c "echo 'addons_path=$ODOO_INSTALL_DIRECTORY_EXT/addons,$ODOO_INSTALL_DIRECTORY/custom/addons' >> /etc/$ODOO_CONFIGFILE_NAME.conf"
 
-echo -e "* Create startup file"
-sudo su root -c "echo '#!/bin/sh' >> $OE_HOME_EXT/start.sh"
-sudo su root -c "echo 'sudo -u $OE_USER $OE_HOME_EXT/openerp-server --config=/etc/$OE_CONFIG.conf' >> $OE_HOME_EXT/start.sh"
-sudo chmod 755 $OE_HOME_EXT/start.sh
+echo -e "\n---- Create startup file"
+sudo su root -c "echo '#!/bin/sh' >> $ODOO_INSTALL_DIRECTORY_EXT/start.sh"
+sudo su root -c "echo 'sudo -u $ODOO_USER $ODOO_INSTALL_DIRECTORY_EXT/openerp-server --config=/etc/$ODOO_CONFIGFILE_NAME.conf' >> $ODOO_INSTALL_DIRECTORY_EXT/start.sh"
+sudo chmod 755 $ODOO_INSTALL_DIRECTORY_EXT/start.sh
 
 #--------------------------------------------------
-# Adding ODOO as a deamon (initscript)
+# ODOO Daemon
 #--------------------------------------------------
-
 echo -e "* Create init file"
-echo '#!/bin/sh' >> ~/$OE_CONFIG
-echo '### BEGIN INIT INFO' >> ~/$OE_CONFIG
-echo '# Provides: $OE_CONFIG' >> ~/$OE_CONFIG
-echo '# Required-Start: $remote_fs $syslog' >> ~/$OE_CONFIG
-echo '# Required-Stop: $remote_fs $syslog' >> ~/$OE_CONFIG
-echo '# Should-Start: $network' >> ~/$OE_CONFIG
-echo '# Should-Stop: $network' >> ~/$OE_CONFIG
-echo '# Default-Start: 2 3 4 5' >> ~/$OE_CONFIG
-echo '# Default-Stop: 0 1 6' >> ~/$OE_CONFIG
-echo '# Short-Description: Enterprise Business Applications' >> ~/$OE_CONFIG
-echo '# Description: ODOO Business Applications' >> ~/$OE_CONFIG
-echo '### END INIT INFO' >> ~/$OE_CONFIG
-echo 'PATH=/bin:/sbin:/usr/bin:/usr/local/bin/' >> ~/$OE_CONFIG
-echo "DAEMON=$OE_HOME_EXT/openerp-server" >> ~/$OE_CONFIG
-echo "NAME=$OE_CONFIG" >> ~/$OE_CONFIG
-echo "DESC=$OE_CONFIG" >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo '# Specify the user name (Default: odoo).' >> ~/$OE_CONFIG
-echo "USER=$OE_USER" >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo '# Specify an alternate config file (Default: /etc/openerp-server.conf).' >> ~/$OE_CONFIG
-echo "CONFIGFILE=\"/etc/$OE_CONFIG.conf\"" >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo '# pidfile' >> ~/$OE_CONFIG
-echo 'PIDFILE=/var/run/$NAME.pid' >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo '# Additional options that are passed to the Daemon.' >> ~/$OE_CONFIG
-echo 'DAEMON_OPTS="-c $CONFIGFILE"' >> ~/$OE_CONFIG
-echo '[ -x $DAEMON ] || exit 0' >> ~/$OE_CONFIG
-echo '[ -f $CONFIGFILE ] || exit 0' >> ~/$OE_CONFIG
-echo 'checkpid() {' >> ~/$OE_CONFIG
-echo '[ -f $PIDFILE ] || return 1' >> ~/$OE_CONFIG
-echo 'pid=`cat $PIDFILE`' >> ~/$OE_CONFIG
-echo '[ -d /proc/$pid ] && return 0' >> ~/$OE_CONFIG
-echo 'return 1' >> ~/$OE_CONFIG
-echo '}' >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo 'case "${1}" in' >> ~/$OE_CONFIG
-echo 'start)' >> ~/$OE_CONFIG
-echo 'echo -n "Starting ${DESC}: "' >> ~/$OE_CONFIG
-echo 'start-stop-daemon --start --quiet --pidfile ${PIDFILE} \' >> ~/$OE_CONFIG
-echo '--chuid ${USER} --background --make-pidfile \' >> ~/$OE_CONFIG
-echo '--exec ${DAEMON} -- ${DAEMON_OPTS}' >> ~/$OE_CONFIG
-echo 'echo "${NAME}."' >> ~/$OE_CONFIG
-echo ';;' >> ~/$OE_CONFIG
-echo 'stop)' >> ~/$OE_CONFIG
-echo 'echo -n "Stopping ${DESC}: "' >> ~/$OE_CONFIG
-echo 'start-stop-daemon --stop --quiet --pidfile ${PIDFILE} \' >> ~/$OE_CONFIG
-echo '--oknodo' >> ~/$OE_CONFIG
-echo 'echo "${NAME}."' >> ~/$OE_CONFIG
-echo ';;' >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo 'restart|force-reload)' >> ~/$OE_CONFIG
-echo 'echo -n "Restarting ${DESC}: "' >> ~/$OE_CONFIG
-echo 'start-stop-daemon --stop --quiet --pidfile ${PIDFILE} \' >> ~/$OE_CONFIG
-echo '--oknodo' >> ~/$OE_CONFIG
-echo 'sleep 1' >> ~/$OE_CONFIG
-echo 'start-stop-daemon --start --quiet --pidfile ${PIDFILE} \' >> ~/$OE_CONFIG
-echo '--chuid ${USER} --background --make-pidfile \' >> ~/$OE_CONFIG
-echo '--exec ${DAEMON} -- ${DAEMON_OPTS}' >> ~/$OE_CONFIG
-echo 'echo "${NAME}."' >> ~/$OE_CONFIG
-echo ';;' >> ~/$OE_CONFIG
-echo '*)' >> ~/$OE_CONFIG
-echo 'N=/etc/init.d/${NAME}' >> ~/$OE_CONFIG
-echo 'echo "Usage: ${NAME} {start|stop|restart|force-reload}" >&2' >> ~/$OE_CONFIG
-echo 'exit 1' >> ~/$OE_CONFIG
-echo ';;' >> ~/$OE_CONFIG
-echo '' >> ~/$OE_CONFIG
-echo 'esac' >> ~/$OE_CONFIG
-echo 'exit 0' >> ~/$OE_CONFIG
+echo '#!/bin/sh' >> ~/$ODOO_CONFIGFILE_NAME
+echo '### BEGIN INIT INFO' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Provides: $ODOO_CONFIGFILE_NAME' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Required-Start: $remote_fs $syslog' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Required-Stop: $remote_fs $syslog' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Should-Start: $network' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Should-Stop: $network' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Default-Start: 2 3 4 5' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Default-Stop: 0 1 6' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Short-Description: Enterprise Business Applications' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Description: ODOO Business Applications' >> ~/$ODOO_CONFIGFILE_NAME
+echo '### END INIT INFO' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'PATH=/bin:/sbin:/usr/bin:/usr/local/bin/' >> ~/$ODOO_CONFIGFILE_NAME
+echo "DAEMON=$ODOO_INSTALL_DIRECTORY_EXT/openerp-server" >> ~/$ODOO_CONFIGFILE_NAME
+echo "NAME=$ODOO_CONFIGFILE_NAME" >> ~/$ODOO_CONFIGFILE_NAME
+echo "DESC=$ODOO_CONFIGFILE_NAME" >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Specify the user name (Default: odoo).' >> ~/$ODOO_CONFIGFILE_NAME
+echo "USER=$ODOO_USER" >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Specify an alternate config file (Default: /etc/openerp-server.conf).' >> ~/$ODOO_CONFIGFILE_NAME
+echo "CONFIGFILE=\"/etc/$ODOO_CONFIGFILE_NAME.conf\"" >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# pidfile' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'PIDFILE=/var/run/$NAME.pid' >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo '# Additional options that are passed to the Daemon.' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'DAEMON_OPTS="-c $CONFIGFILE"' >> ~/$ODOO_CONFIGFILE_NAME
+echo '[ -x $DAEMON ] || exit 0' >> ~/$ODOO_CONFIGFILE_NAME
+echo '[ -f $CONFIGFILE ] || exit 0' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'checkpid() {' >> ~/$ODOO_CONFIGFILE_NAME
+echo '[ -f $PIDFILE ] || return 1' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'pid=`cat $PIDFILE`' >> ~/$ODOO_CONFIGFILE_NAME
+echo '[ -d /proc/$pid ] && return 0' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'return 1' >> ~/$ODOO_CONFIGFILE_NAME
+echo '}' >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'case "${1}" in' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'start)' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo -n "Starting ${DESC}: "' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'start-stop-daemon --start --quiet --pidfile ${PIDFILE} \' >> ~/$ODOO_CONFIGFILE_NAME
+echo '--chuid ${USER} --background --make-pidfile \' >> ~/$ODOO_CONFIGFILE_NAME
+echo '--exec ${DAEMON} -- ${DAEMON_OPTS}' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo "${NAME}."' >> ~/$ODOO_CONFIGFILE_NAME
+echo ';;' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'stop)' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo -n "Stopping ${DESC}: "' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'start-stop-daemon --stop --quiet --pidfile ${PIDFILE} \' >> ~/$ODOO_CONFIGFILE_NAME
+echo '--oknodo' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo "${NAME}."' >> ~/$ODOO_CONFIGFILE_NAME
+echo ';;' >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'restart|force-reload)' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo -n "Restarting ${DESC}: "' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'start-stop-daemon --stop --quiet --pidfile ${PIDFILE} \' >> ~/$ODOO_CONFIGFILE_NAME
+echo '--oknodo' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'sleep 1' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'start-stop-daemon --start --quiet --pidfile ${PIDFILE} \' >> ~/$ODOO_CONFIGFILE_NAME
+echo '--chuid ${USER} --background --make-pidfile \' >> ~/$ODOO_CONFIGFILE_NAME
+echo '--exec ${DAEMON} -- ${DAEMON_OPTS}' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo "${NAME}."' >> ~/$ODOO_CONFIGFILE_NAME
+echo ';;' >> ~/$ODOO_CONFIGFILE_NAME
+echo '*)' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'N=/etc/init.d/${NAME}' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'echo "Usage: ${NAME} {start|stop|restart|force-reload}" >&2' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'exit 1' >> ~/$ODOO_CONFIGFILE_NAME
+echo ';;' >> ~/$ODOO_CONFIGFILE_NAME
+echo '' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'esac' >> ~/$ODOO_CONFIGFILE_NAME
+echo 'exit 0' >> ~/$ODOO_CONFIGFILE_NAME
 
-echo -e "* Security Init File"
-sudo mv ~/$OE_CONFIG /etc/init.d/$OE_CONFIG
-sudo chmod 755 /etc/init.d/$OE_CONFIG
-sudo chown root: /etc/init.d/$OE_CONFIG
+echo -e "\n---- Security Init File"
+sudo mv ~/$ODOO_CONFIGFILE_NAME /etc/init.d/$ODOO_CONFIGFILE_NAME
+sudo chmod 755 /etc/init.d/$ODOO_CONFIGFILE_NAME
+sudo chown root: /etc/init.d/$ODOO_CONFIGFILE_NAME
 
-echo -e "* Start ODOO on Startup"
-sudo update-rc.d $OE_CONFIG defaults
+echo -e "\n---- Start ODOO on Startup"
+sudo update-rc.d $ODOO_CONFIGFILE_NAME defaults
  
-echo "Done! The ODOO server can be started with /etc/init.d/$OE_CONFIG"
+echo "Done! The ODOO server can be started with /etc/init.d/$ODOO_CONFIGFILE_NAME"
 
 
